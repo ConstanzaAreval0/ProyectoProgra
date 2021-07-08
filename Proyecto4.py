a6f0dc34737fcf08d4789b45c47dabc1682f06ba
ConstanzaAreval0 <87148926+ConstanzaAreval0@users.noreply.github.com>
2021-07-08 18:29:01 -0400

    Update Proyecto4.py

--- Proyecto4.py
+++ Proyecto4.py
@@ -1,6 +1,5 @@
 #Proyecto Programación 4.
 
-def leer_datos (nombre_archivo):#Proyecto4.py
-  
-  archivo = open("CasosGeneroEtarioEtapaClinicaa_std.csv","r")
-  archivo = archivo.readlines()
+#Abrir el Archivo.
+archivo = open("CasosGeneroEtarioEtapaClinica_std.csv","r")
+archivo = archivo.readlines()
