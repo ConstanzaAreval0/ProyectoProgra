003fc9b830dc09185d6f5f24481def503f4f3110
ConstanzaAreval0 <constanza.arevalof@alumnos.uv.cl>
2021-07-08 18:49:37 -0400

    Update Proyecto4.py

--- Proyecto4.py
+++ Proyecto4.py
@@ -1,5 +1,16 @@
 #Proyecto Programación 4.
 
+
 #Abrir el Archivo.
-archivo = open("CasosGeneroEtarioEtapaClinica_std.csv","r")
-archivo = archivo.readlines()
+archivo = open("CasosGeneroEtarioEtapaClinica_std.csv", "r")
+
+#Leer el archivo
+def Leer(archivo):
+    leer = archivo.readlines()
+    for i in leer:
+        print(i)
+
+print(Leer(archivo))
+    
+#Cerrar el Archivo
+archivo.close()
