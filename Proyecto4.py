78d0d5ff2b7e1e514e24b260a1271d92d2cadfe4
ConstanzaAreval0 <87148926+ConstanzaAreval0@users.noreply.github.com>
2021-07-11 17:43:39 -0400

    Update Proyecto4.py

--- Proyecto4.py
+++ Proyecto4.py
@@ -1,20 +1,6 @@
 #Proyecto Programación 4.
 
 
-#Abrir el Archivo.
-archivo = open("CasosGeneroEtarioEtapaClinica_std.csv", "r")
-
-#Leer el archivo
-def Leer(archivo):
-    leer = archivo.readlines()
-    for i in leer:
-        print(i)
-
-print(Leer(archivo))
-    
-#Cerrar el Archivo
-archivo.close()
-
 #importar librerias a utilizar.
 import pandas as pd
 import numpy as mp
